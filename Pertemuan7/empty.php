bf72e84efae89539afb904648817ebebc901dc97
Andreas Gale Dwi Jaya <andreasgaledwijaya@gmail.com>
2024-04-01 21:12:37 +0700

    Pertemuan 7 - Praktikum 2

---
+++ Pertemuan7/empty.php
@@ -0,0 +1,15 @@
+<?php
+$myArray = array(); // array kosong
+
+if(empty($myArray)){
+    echo "Array tidak terdefinisi atau kosong <br>";
+}else{
+    echo "Array terdefinisi dan tidak kosong";
+}
+
+if(empty($nonExistentVar)){
+    echo "Variabel tidak terdefinisi atau kosong";
+}else{
+    echo "Variabel terdefinisi dan tidak kosong";
+}
+?>
